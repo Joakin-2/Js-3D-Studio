4380da9b8b86cfc3423ba19f84f56bb051032b07
Jok <82481025+Joakin-2@users.noreply.github.com>
2026-04-11 19:56:38 +0000

    new

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ const produtos = [
     //new { id: 26, nome: "Galinha do Minecraft 3D", preco: 39.90, img: "https://down-br.img.susercontent.com/file/br-11134207-820ly-mlz5h9svf4lj6c.webp"},
     //new { id: 27, nome: "Suporte de Celular de Mesa", preco: 18.90, img: "https://down-br.img.susercontent.com/file/br-11134207-820m7-mmayuf68c83q05.webp" },
 
-    //new { id: 28, nome: "Kit com 10 Prendedores", preco: 18.50, img: "https://down-br.img.susercontent.com/file/br-11134207-820le-mmv3amm94r9ca3@resize_w450_nl.webp"},
+    //new { id: 28, nome: "Kit com 10 Prendedores", preco: 18.50, img: "https://down-br.img.susercontent.com/file/br-11134207-820lk-mmxicqyhxlhd80@resize_w450_nl.webp"},
     //new { id: 29, nome: "Escultura de Jesus 3D", preco: 31.90, img: "https://down-br.img.susercontent.com/file/br-11134207-820l8-mmxicqy6xhc1a7.webp"},
     //{ id: 30, nome: "Flores Minecraft Decorativas", preco: 69.90, img: "https://down-br.img.susercontent.com/file/br-11134207-820lb-mly72z7s4jk4a0.webp" },
 
